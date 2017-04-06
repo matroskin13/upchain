b6c552e77c1f05f1577503daefda60849150d3b9
valentin.dubrovskiy <v.dubrovskiy@rambler-co.ru>
2017-04-07 00:02:08 +0300

    add errors

--- examples/users.js
+++ examples/users.js
@@ -1,10 +1,12 @@
 const lodash = require('lodash');
-const { createService, HttpAdapter } = require('../');
+const { createService, HttpAdapter, createError, LEVEL_NOTICE } = require('../');
 
 let userService = createService({ port: 3011 }, HttpAdapter);
 
 userService.all('usersIds', (property, input, payload) => {
     payload.users = property.map(id => ({ id, name: 'user' + id }));
 });
+
+userService.all('error', () => createError('invalid_user', LEVEL_NOTICE));
 
 userService.start();
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const HttpAdapter = require('./lib/http-adapter');
 const Cluster = require('./lib/cluster');
 const Service = require('./lib/service');
 
+const constants = require('./lib/constants');
+
 const createCluster = ({ port, members = [], adapter }) => new Cluster(port, members, adapter);
 const createService = (options, adapter) => new Service(options, adapter);
 
 exports.createCluster = createCluster;
 exports.createService = createService;
 exports.HttpAdapter = HttpAdapter;
+
+Object.assign(exports, constants);
+
+exports.createError = (code, level) => Promise.reject({ code, level });
